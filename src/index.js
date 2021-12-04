f6c57a8bdac658ea2b61b8e621f2c643cff97e9a
jasonronalddavis <jdavis@c.ringling.edu>
2021-12-04 12:59:46 -0600

    Done.

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import counterReducer from './reducers/counterReducer.js';
 import App from './App';
 import './index.css';
 
-const store = createStore(counterReducer); 
+const store = createStore(counterReducer,  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()); 
 
 ReactDOM.render(
 <App store={store} />, 
